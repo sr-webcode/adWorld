c423c5dbf1eb581415b38dbbaf15454f40beea37
sean <sr.webcode@gmail.com>
2021-01-30 20:05:46 +0800

    [update] simple spinner during record fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { Typography } from "antd";
+import { Typography, Spin } from "antd";
 
 import { IAdEvents } from "types/adWorld";
 import TrackEvents from "components/TrackEvents";
@@ -12,8 +12,10 @@ const StyledHeader = styled.div`
 `;
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   const [trackEvents, setTrackEvents] = useState<IAdEvents[]>([]);
   const fetchAdEvents = async () => {
+    setLoading(true);
     try {
       const adRequest = await fetch(
         "https://adworld-cms.istackmanila.com/tracks"
@@ -23,6 +25,7 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   };
   useEffect(() => {
     fetchAdEvents();
@@ -38,6 +41,11 @@ const App = () => {
           IT'S LIKE 13 EVENTS IN ONE.
         </Title>
       </StyledHeader>
+      {loading && (
+        <div className="py-5 w-100 d-flex justify-content-center">
+          <Spin />
+        </div>
+      )}
       {Boolean(trackEvents.length) && <TrackEvents events={trackEvents} />}
     </div>
   );
