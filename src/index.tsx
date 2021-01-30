c88774bb813c77b3b810ec18b1141998b4d8d2db
sean <sr.webcode@gmail.com>
2021-01-30 10:58:51 +0800

    [update] stylesheet third party style imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "antd/dist/antd.css";
+import "bootstrap-utilities/bootstrap-utilities.css";
 import "./stylesheets/index.css";
 
 import App from "./App";
